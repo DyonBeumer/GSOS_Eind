b126c6cedad97a395fe8ba707ef76854c6d84dab
Dyon Beumer <Dyonunic@gmail.com>
2016-05-19 14:08:38 +0200

    new

--- src/businesslogic/BusinessLogic.java
+++ src/businesslogic/BusinessLogic.java
@@ -2,4 +2,14 @@ package businesslogic;
 
 public class BusinessLogic{
 
+    public String ProcessLocation(String location){
+        //Send to google
+        GoogleConsumer.class.
+
+        //Send to weatherapi
+
+
+        //Process response
+
+    }
 }
---
+++ src/businesslogic/GoogleConsumer.java
@@ -0,0 +1,16 @@
+package businesslogic;
+
+public GoogleConsumer {
+        private static String url = "maps.googleapis.com/maps/api/geocode/json?";
+        private static String key = "AIzaSyAZm3KQ8kbZkw2Cu6TYovuMx9_G4wXcVco";
+
+
+        public JSON aquireGeometry(String adress){
+
+            tempurl = url + "address="+location + "&key="+key;
+            URL test = new URL(tempurl);
+
+        }
+
+
+}
