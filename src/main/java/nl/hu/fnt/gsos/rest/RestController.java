f49ce4f4217aacf9bf1d34fb2059a6c53feb8734
Arjan van Maren <Arjanvanmaren@gmail.com>
2016-05-19 15:36:41 +0200

    Merge branch 'master' of https://github.com/DyonBeumer/GSOS_Eind
    # Conflicts:
    #	target/classes/nl/hu/fnt/gsos/rest/Track.class
    #	target/les-8-rest.war
    #	target/maven-archiver/pom.properties
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/nl/hu/fnt/gsos/rest/RestController.java
+++ src/main/java/nl/hu/fnt/gsos/rest/RestController.java
@@ -1,11 +1,8 @@
 package nl.hu.fnt.gsos.rest;
 
 
-import java.util.ArrayList;
 
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -19,8 +16,7 @@ public static src.businesslogic.BusinessLogicController b = new src.businesslogi
 	@Path("{stad}")
 	@Produces({MediaType.APPLICATION_JSON})
 	public String getTracks(@PathParam("stad") String stad) {
-		b
 
-
+		return "";
      }
 }
