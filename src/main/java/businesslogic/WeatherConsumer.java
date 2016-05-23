2e3c5b38465d70a419d60df2bff30b4ca58f139f
Arjan van Maren <Arjanvanmaren@gmail.com>
2016-05-23 12:46:09 +0200

    rest in ui fixed

--- src/main/java/businesslogic/WeatherConsumer.java
+++ src/main/java/businesslogic/WeatherConsumer.java
@@ -1,65 +1,94 @@
 package businesslogic;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import javax.xml.soap.*;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
 
 public class WeatherConsumer {
     private static String url = "api.openweathermap.org/data/2.5/weather?";
     private static String key = "45f11f8c4e48c92c287cc43cf2fe8f7e";
-    public final static String DEFAULT_SERVER
-            = "http://www.elharo.com/fibonacci/SOAP";
-    public final static String SOAP_ACTION
-            = "http://www.example.com/fibonacci";
 
 
-    public String aquireWeather(String lat, String lon) {
-        String server = DEFAULT_SERVER;
 
+    public String aquireWeather(String zip) {
         try {
-            URL u = new URL(server);
-            URLConnection uc = u.openConnection();
-            HttpURLConnection connection = (HttpURLConnection) uc;
+            // Create SOAP Connection
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
 
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("SOAPAction", SOAP_ACTION);
+            // Send SOAP Message to SOAP Server
+            String url = "http://www.webservicex.net/weatherforecast.asmx";
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(zip), url);
 
-            OutputStream out = connection.getOutputStream();
-            Writer wout = new OutputStreamWriter(out);
+            // Process the SOAP Response
+            printSOAPResponse(soapResponse);
 
-            wout.write("<?xml version='1.0'?>\r\n");
-            wout.write("<SOAP-ENV:Envelope ");
-            wout.write("xmlns:SOAP-ENV=");
-            wout.write(
-                    "'http://schemas.xmlsoap.org/soap/envelope/' "
-            );
-            wout.write("xmlns:xsi=");
-            wout.write(
-                    "'http://www.w3.org/2001/XMLSchema-instance'>\r\n");
-            wout.write("  <SOAP-ENV:Body>\r\n");
-            wout.write("    <calculateFibonacci ");
-            wout.write(
-                    "xmlns='http://namespaces.cafeconleche.org/xmljava/ch3/'\r\n"
-            );
-            wout.write("    type='xsi:positiveInteger'>" + input
-                    + "</calculateFibonacci>\r\n");
-            wout.write("  </SOAP-ENV:Body>\r\n");
-            wout.write("</SOAP-ENV:Envelope>\r\n");
-
-            wout.flush();
-            wout.close();
-
-            InputStream in = connection.getInputStream();
-            int c;
-            while ((c = in.read()) != -1) System.out.write(c);
-            in.close();
-
+            soapConnection.close();
+        } catch (Exception e) {
+            System.err.println("Error occurred while sending SOAP Request to Server");
+            e.printStackTrace();
         }
-        catch (IOException e) {
-            System.err.println(e);
-        }
+        return "";
     }
+
+
+
+
+    private static SOAPMessage createSOAPRequest(String zip) throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        String serverURI = "http://www.webservicex.net/GetWeatherByZipCode";
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        //envelope.addNamespaceDeclaration("example", serverURI);
+
+        /*
+        Constructed SOAP Request Message:
+        <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:example="http://ws.cdyne.com/">
+            <SOAP-ENV:Header/>
+            <SOAP-ENV:Body>
+                <example:VerifyEmail>
+                    <example:email>mutantninja@gmail.com</example:email>
+                    <example:LicenseKey>123</example:LicenseKey>
+                </example:VerifyEmail>
+            </SOAP-ENV:Body>
+        </SOAP-ENV:Envelope>
+         */
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement soapBodyElem = soapBody.addChildElement("GetWeatherByZipCode");
+        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("ZipCode");
+        soapBodyElem1.addTextNode(zip);
+
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", serverURI);
+
+        soapMessage.saveChanges();
+
+        /* Print the request message */
+        System.out.print("Request SOAP Message = ");
+        soapMessage.writeTo(System.out);
+        System.out.println();
+
+        return soapMessage;
+    }
+
+    /**
+     * Method used to print the SOAP Response
+     */
+    private static void printSOAPResponse(SOAPMessage soapResponse) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        Source sourceContent = soapResponse.getSOAPPart().getContent();
+        System.out.print("\nResponse SOAP Message = ");
+        StreamResult result = new StreamResult(System.out);
+        transformer.transform(sourceContent, result);
+    }
+
 }
